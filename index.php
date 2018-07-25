2fc84015c0837e72780159096a4271f99c83ae54
dembasy <dembasy1227@gmail.com>
2018-07-25 15:54:14 +0200

    done

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>exo4</title>
+  </head>
+  <body>
+    <p><?php $int = 'lol'; echo $int; ?></p>
+  </body>
+</html>
